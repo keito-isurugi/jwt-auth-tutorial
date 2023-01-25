c87e136f1381d23e565ab8b8b4e7954ee2ca616c
keito-isurugi <ja224kg@gmail.com>
2023-01-26 08:04:15 +0900

    JWT発行

---
+++ db/User.js
@@ -0,0 +1,12 @@
+const User = [
+	{
+		email: "test@email.com",
+		password: "123456",
+	},
+	{
+		email: "hoge@email.com",
+		password: "123123",
+	},
+]
+
+module.exports = { User }
--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,22 @@
 const router = require("express").Router()
 const { body, validationResult } = require('express-validator');
+const { User } = require("../db/User")
+const bcrypt = require("bcrypt")
+const JWT = require("jsonwebtoken");
+const { application } = require("express");
 
 router.get("/", (req, res) => {
 	res.send("Hello Authjs")
 })
 
 // ユーザー新規登録用のAPI
+// email, passwordの入力
 router.post(
 	"/register", 
+	// バリデーションチェック
 	body("email").isEmail(), 
 	body("password").isLength({min: 6}),
-	(req, res) => {
+	async (req, res) => {
 	const email = req.body.email
 	const password = req.body.password
 
@@ -19,7 +25,82 @@ router.post(
 		return res.status(400).json({ errors: errors.array() })
 	}
 
-	// console.log(email, password)
+	// DBにデータが存在してるか確認
+	const user = User.find((res) => res.email === email)
+	if(user) {
+		return res.status(400).json([
+			{
+				message: "すでにそのユーザーは存在しています。"
+			}, 
+		])
+	}
+
+	// パスワードの暗号化
+	let hashedPassword = await bcrypt.hash(password, 10)
+	// console.log(hashedPassword)
+
+	// DBへの保存
+	User.push({
+		email,
+		password: hashedPassword
+	})
+
+	// JWT発行
+	const token = await JWT.sign(
+		{
+			email,
+		},
+		"SECRET_KEY",
+		{
+			expiresIn: "24h",
+		}
+	)
+
+	return res.json({
+		token: token
+	})
+})
+
+// ログイン用のAPI
+router.post("/login", async (req, res) => {
+	const { email, password } = req.body
+
+	const user = User.find((user) => user.email === email)
+	if(!user) {
+		return res.status(400).json([
+			{
+				message: "そのユーザーは存在しません"
+			}, 
+		])
+	}
+
+	// パスワードの復号、照合
+	const isMatch = await bcrypt.compare(password, user.password)
+
+	if(!isMatch) {
+		return res.status(400).json([
+			{
+				message: "パスワードが異なります"
+			}, 
+		])
+	}
+	const token = await JWT.sign(
+		{
+			email,
+		},
+		"SECRET_KEY",
+		{
+			expiresIn: "24h",
+		}
+	)
+	return res.json({
+		token: token
+	})
+})
+
+// DBのユーザーを確認するAPI
+router.get("/allUsers", (req, res) => {
+	return res.json(User)
 })
 
 module.exports = router
