2121d8abaf32b63c8aa5c105e53a22ce1a8c6cca
keito-isurugi <ja224kg@gmail.com>
2023-01-25 21:18:50 +0900

    express-validator導入

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,25 @@
 const router = require("express").Router()
+const { body, validationResult } = require('express-validator');
 
 router.get("/", (req, res) => {
 	res.send("Hello Authjs")
 })
 
 // ユーザー新規登録用のAPI
-router.post("/register", (req, res) => {
+router.post(
+	"/register", 
+	body("email").isEmail(), 
+	body("password").isLength({min: 6}),
+	(req, res) => {
 	const email = req.body.email
 	const password = req.body.password
 
-	console.log(email, password)
+	const errors = validationResult(req)
+	if(!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() })
+	}
+
+	// console.log(email, password)
 })
 
 module.exports = router
